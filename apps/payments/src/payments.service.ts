33c91384c4937170c4ec871d7bd2ab918f0cdce8
RakulAgn <rakul@cartrabbit.in>
2025-05-06 05:27:38 +0530

    feat(payments): add createCharge method and CreateChargeDto for payment processing

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
+import { CreateChargeDto } from './dto/create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
@@ -12,4 +13,21 @@ export class PaymentsService {
   );
 
   constructor(private readonly configService: ConfigService) {}
+
+  async createCharge({ amount, card }: CreateChargeDto) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card,
+    });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      amount: amount * 100,
+      currency: 'usd',
+      confirm: true,
+      payment_method_types: ['card'],
+      payment_method: paymentMethod.id,
+    });
+
+    return paymentIntent;
+  }
 }
